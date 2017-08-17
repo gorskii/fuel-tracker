41e98ac73bc44f35b3d54f97a80b1a5ca47a5717
Egor Yakubov <egor.yakayaka@gmail.com>
2017-08-17 18:30:59 +0400

    Fixed configuration for login page

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,7 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Authorization settings
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
